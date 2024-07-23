4a60fb3f10aaf81d787b7f6abb5f0b9b41a9b34e
liyaso24-meet <liyasol0711@gmail.com>
2024-07-23 16:05:09 +0300

    Finished 2 forms
    have left to add a comment list to the data base

--- MeetCrave/app.py
+++ MeetCrave/app.py
@@ -42,10 +42,11 @@ def login():
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             login_session['crave'] = []
-            return redirect(url_for('home'))
+            return redirect(url_for('crave'))
 
-        except:
+        except Exception as e:
             error = "login failed, try again."
+            print(e)
             return render_template("login.html", error=error)
 
 
@@ -66,21 +67,41 @@ def signup():
             db.child("users").child(UID).set(user)
             login_session['crave'] = []
             return redirect(url_for('crave'))
-        except:
+        except Exception as e:
             error = "Authentication error"
+            print(e)
             return render_template("signup.html",error=error)
         
 #app route Crave
-@app.route('/crave')
+@app.route('/crave', methods= ['GET', 'POST'])
 def crave():
-    return render_template("crave.html")
+    if request.method == 'GET':
+        return render_template("crave.html") 
+    else: 
+        UID = login_session['user']['localId']
+        crave = {"crave":request.form['crave'], "whocraves": request.form['whocraves'], "comments": {}}
+        db.child('crave').push(crave)
+        return redirect(url_for('gallary'))
 
 #app route gallary
-@app.route('/gallary')
+@app.route('/gallary', methods= ['GET', 'POST'])
 def gallary():
-    return render_template("gallary.html")
+    if request.method == 'GET':
+        crave = db.child("crave").get().val()
+        return render_template("gallary.html", crave= crave)
 
+    #else:
+       # UID = login_session['user']['localId']
+        #crave = {"crave":request.form['crave'], "whocraves": request.form['whocraves'], "comments": {}}
+        #db.child('crave').push(crave)
+        #return render_template("gallary.html")
 
+#app route signout
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('main'))
 
 
 
